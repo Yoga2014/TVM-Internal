a9f22d497f839e50174cb9ed8f1d22d00f58b512
Varsha <varshatom7@gmail.com>
2024-08-13 20:29:57 +0530

    Overview  scree static page

--- fm/src/app/overview/overview.component.ts
+++ fm/src/app/overview/overview.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrl: './overview.component.scss'
+  styleUrl: './overview.component.scss',
 })
 export class OverviewComponent {
+  selectedTab: any = '';
+  bootstrap: any;
 
+
+  aditionalClick(getValues: any) {
+    this.selectedTab = getValues;
+  }
 }
