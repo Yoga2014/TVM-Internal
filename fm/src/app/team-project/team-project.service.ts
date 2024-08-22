9217b651a6c449e9db43fde690862fb3cce4fd2b
VanmathiSambandham <vanmathis2396@gmail.com>
2024-08-22 15:52:17 +0530

    changes 02

--- fm/src/app/team-project/team-project.service.ts
+++ fm/src/app/team-project/team-project.service.ts
@@ -6,30 +6,30 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class TeamProjectService {
-
-  api='http://localhost:3000/projectForm'
-
  
+  api='http://localhost:3002/project'
+  constructor(private apihttp:HttpClient) { }
   
-  
-  
-    constructor(private apihttp:HttpClient) { }
-  
-    
-  
-    
-    getMethod(){
-      return this.apihttp.get('http://localhost:3000/projectForm')
+  getMethod(){
+      return this.apihttp.get('http://localhost:3002/project')
     }
 
   
   
   postMethod(item:any){
-    return this.apihttp.post('http://localhost:3000/projectForm',item)
+    return this.apihttp.post('http://localhost:3002/project',item)
   }
 
   
-  
-  
+  deleteMethod(id:any){
+    return this.apihttp.delete('http://localhost:3002/project'+'/'+id)
+  }
 
-}
+
+    editMethod(id:any){
+      return this.apihttp.get('http://localhost:3002/project'+'/'+id);
+      }
+    updateMethod(id:any,data:any){
+         return this.apihttp.put('http://localhost:3002/project'+'/'+id,data);
+       }
+  }
