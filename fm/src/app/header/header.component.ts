4d6c3817912507f102cf6262837320704fbda256
Varsha <varshatom7@gmail.com>
2024-08-27 16:40:16 +0530

    alignment changes

--- fm/src/app/header/header.component.ts
+++ fm/src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { slideInAnimation } from './animation';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -14,6 +14,12 @@ export class HeaderComponent {
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  activeTab: string = 'general';
+
+  selectTab(tabName: string) {
+    this.activeTab = tabName;
   }
 
 }
